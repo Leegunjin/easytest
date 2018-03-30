9d0f889fb827e19089d173c4c4a156ffa8c66e88
gjlee <gjlee@LAPTOP-D2TG0CSB>
2018-03-30 16:32:06 +0900

    0330 16:31 구글 로그인 병합완료

--- FinalProject/src/main/java/com/scitmaster/easycodingu/pereson/controller/PersonViewController.java
+++ FinalProject/src/main/java/com/scitmaster/easycodingu/pereson/controller/PersonViewController.java
@@ -74,12 +74,6 @@ public class PersonViewController {
 		return "person/forgotForm";
 	}
 	
-	
-	
-	
-	
-	
-	
 	// 180328 로그인 폼에서 회원가입 폼으로 이동 (주 지호)
 	@RequestMapping(value="joinForm",method=RequestMethod.GET)
 	public String joinForm(){
@@ -140,22 +134,22 @@ public class PersonViewController {
 	   }
 	
 	   
-		/**
-		 * googleUser
-		 * @param request 사용자의 구글 계정으로부터 가져온 이름과 Email정보 
-		 * @param session 로그인한 사용자 정보를 session에 저장
-		 * */
-		@RequestMapping(value="/googleUser", method = RequestMethod.GET)
-		public String successGoogleLogin(ServletRequest request, HttpSession session) {
-			
-			String name=(String)request.getParameter("name");
-			String email=(String)request.getParameter("email");
-			
-			logger.info(name);
-			logger.info(email);
-			
-			return "redirect:../";
-		}
+	/**
+	 * googleUser
+	 * @param request 사용자의 구글 계정으로부터 가져온 이름과 Email정보 
+	 * @param session 로그인한 사용자 정보를 session에 저장
+	 * */
+	@RequestMapping(value="/googleUser", method = RequestMethod.GET)
+	public String successGoogleLogin(ServletRequest request, HttpSession session) {
+		
+		String name=(String)request.getParameter("name");
+		String email=(String)request.getParameter("email");
+		
+		logger.info(name);
+		logger.info(email);
+		
+		return "redirect:../";
+	}
 
 
 	  
